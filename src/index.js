15248d1989c0a9777fefbde02cfbb10663ea1ed6
Anesu24 <anesuchifamba24@gmail.com>
2024-11-01 13:24:56 +0200

    added apiurl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+function searchCity(city) {
+    let apiKey = "38ceca6c8obf3e60a0794bbb2d9at245";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit= metric';
+    console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     if (searchInput) {
         let cityElement = document.querySelector("#city");
         cityElement.innerHTML = searchInput.value;
+        searchCity(searchInput.value);
     } else {
         console.error("Input element with ID 'search-form-input' not found.");
     }
