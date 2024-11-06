ebeb9ab248fd2d43f8b921f4545995b0b59de91e
Anesu24 <anesuchifamba24@gmail.com>
2024-11-06 18:29:41 +0200

    updated html,css and weather data

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let newCity = document.querySelector("#weather-app-city");
+  let weatherDescription = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  newCity.innerHTML = response.data.city;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
-    let apiKey = "38ceca6c8obf3e60a0794bbb2d9at245";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit= metric';
-    console.log(apiUrl);
+  let apiKey = "3deb07c475cdt015b4f399a253e4o0b8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    if (searchInput) {
-        let cityElement = document.querySelector("#city");
-        cityElement.innerHTML = searchInput.value;
-        searchCity(searchInput.value);
-    } else {
-        console.error("Input element with ID 'search-form-input' not found.");
-    }
-}
-
-let searchElement = document.querySelector("#search-form");
-if (searchElement) {
-    searchElement.addEventListener("submit", handleSearchSubmit);
-} else {
-    console.error("Search form element not found.");
+function findCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+searchFormElement.addEventListener("submit", findCity);
 
-
+searchCity("Madrid");
